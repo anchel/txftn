e9846dbeced10413c7ed834fa9bbbea0d3e52909
unknown <ancheltong@.tencent.com>
2015-06-13 21:28:52 +0800

    update

--- src/modules/common/util.js
+++ src/modules/common/util.js
@@ -17,6 +17,20 @@
                 console.log(msg);
             }
         },
+        extend : function(){
+            var arr = Array.prototype.slice.call(arguments);
+            if(arr.length < 2) return arr[0];
+            var target = arr[0];
+            for(var i=1,len=arr.length; i<len; i++){
+                var tmp = arr[i];
+                for(var m in tmp){
+                    if(tmp.hasOwnProperty(m)){
+                        target[m] = tmp[m];
+                    }
+                }
+            }
+            return target;
+        },
         inherit : function(childClass, parentClass) {
             var fn = function() {
             };
--- src/modules/upload/upload.main.js
+++ src/modules/upload/upload.main.js
@@ -14,13 +14,82 @@
     var EventType = CONST_DEF.EventType;
     var AlgType = CONST_DEF.AlgType;
     
+    var emptyFn = function(){};
+    
+    var $ = window.jQuery;
+    
+    var defaultOptions = {
+        onSelect : emptyFn,
+        onStart : emptyFn,
+        onScanProgress : emptyFn,
+        onUploadStart : emptyFn,
+        onUploadProgress : emptyFn,
+        onSuccess : emptyFn,
+        onError : emptyFn,
+        
+        
+        triggerSelector : '.upload_file_input'
+    };
+    
+    var uploadCoreObjMap = {
+        
+    };
+    
+    var _options = {
+        
+    };
+    
+    function createUploadCore(file){
+        var uniqueKey = Util.getUniqueKey();
+        var fio = {
+            uniqueKey : uniqueKey,
+            file : file
+        };
+        var uc = new FTN_H5.UploadCore(fio);
+        uploadCoreObjMap[uniqueKey] = uc;
+        return {
+            uniqueKey : uniqueKey,
+            name : file.name,
+            size : file.size
+        };
+    }
+    
+    function initEvent(){
+        $(document).on('change', _options.triggerSelector, function(e){
+            var oe = e.originalEvent;
+            var files = e.target.files;
+            if(!files || files.length==0){
+                return;
+            }
+            for(var i=0,len=files.length; i<len; i++){
+                var fio = createUploadCore(files[i]);
+                _options.onSelect(fio);
+            }
+        });
+    }
+    
     var UploadMain = {
-        init : function(){
+        
+        inited : false,
+        
+        init : function(options){
+            if(this.inited) return;
+            options = options || {};
+            
+            Util.extend(_options, defaultOptions, options);
+            initEvent();
+        },
+        
+        start : function(fio){
+            
+        },
+        
+        cancel : function(fio){
             
         },
         
         isSupport : function(){
-            
+            return true;
         }
     };
     
